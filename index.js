fead9c005df74e138e2e3632af68cd76a14008dd
Mory Doumbia <mory.d192@gmail.com>
2020-07-12 23:14:47 -0400

    updated index.js

--- index.js
+++ index.js
@@ -3,68 +3,59 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 // const generateMarkdown = require("./utils/generateMarkdown.js");
 
-
-
 const questions = [
-    //ask user for name of the app 
     {
-      
-        type: 'input',
-        name: 'repoName',
-        message: 'What is name of this application? (This will aslo the main header of the README)',
-     
+        "message": "What is your github id?",
+        "type": "input",
+        "name": "username"
     },
-    //asks user for git hub user name 
     {
-        type: 'input',
-        name: 'gitHubName',
-        message: 'What is your username on Github?',
-     
+        "message": "What's the project repo name?",
+        "type": "input",
+        "name": "project"
     },
-    // asks for a description of the project 
     {
-        type: 'input',
-        message: 'Write a description of this application',
-        name: 'description',
-     
+        "message": "What's the project title?",
+        "type": "input",
+        "name": "projectTitle",
+        "default": "No Project Title Provided"
     },
-    // How do you install your app? 
     {
-        type: 'input',
-        message: 'How does a user install your application? Provide instuctions here',
-        name: 'installApp',
-     
+        "message": "What's the project description?",
+        "type": "input",
+        "name": "projectDescription",
+        "default": "No Description Provided"
     },
-   // How does a user use your application? 
     {
-        type: 'input',
-        message: 'How does a user use your application? Provide instuctions here',
-        name: 'appInstructions',
-     
+        "message": "What are the installation instructions?",
+        "type": "input",
+        "name": "installationInsctructions",
+        "default": "No Installation Instructions Provided"
     },
-    // How would a user of this application report an issue, or make a contribution to it? 
     {
-        type: 'input',
-        message: 'Would you like you users to be able to report issues or contribute to this project? add instructions here.',
-        name: 'appIssuesAndCont',
-     
+        "message": "How do you run the application?",
+        "type": "input",
+        "name": "usage",
+        "default": "No Usage Instructions Provided"
     },
- // promts what license they would you  like to use? 
- // THIS SHOULD BE USED FOR A BADGE. 
     {
-        type: 'input',
-        message: 'What license they would you  like to use? (Ex: MIT, GPL 3.0) If you do not wish to use a license type "none" ',
-        name: 'license',
-     
+        "message": "Which license do you want to use for this project?",
+        "type": "list",
+        "name": "license",
+        "default": "MIT License"
     },
- //Asks the user who else contributed to their application 
     {
-        type: 'input',
-        message: 'Who else contributed to your application? if none write "none" ',
-        name: 'contributors',
-     
+        "message": "How do you run the tests?",
+        "type": "input",
+        "name": "tests",
+        "default": "No Testing"
     },
-
+    {
+        "message": "What is the email address of the repo owner?",
+        "type": "input",
+        "name": "email",
+        "default":  "mory.d192@gmail.com"
+    },
 ];
 
 function init() {
